5c9989890618b8057520023ce60932610ec672cb
Jason Toups <jason.p.toups@gmail.com>
2018-04-05 12:07:48 -0400

    create index view and get route

--- controllers/questions.js
+++ controllers/questions.js
@@ -1,3 +1,11 @@
 const router = require('express').Router()
 const db = require('../db/connection')
+const Question = require('../models/question.js')
+
+router.get('/', (req, res) => {
+  Question
+    .find({})
+    .then((questions) => res.render('list', {questions}))
+})
+
 module.exports = router
