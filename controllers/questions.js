a11bf2802ceeee68ef0d17e33acdbe033fecfe63
Meron Kebede <kebedemeron905@gmail.com>
2018-04-05 12:39:34 -0400

    added new view

--- controllers/questions.js
+++ controllers/questions.js
@@ -8,4 +8,12 @@ router.get('/', (req, res) => {
     .then((questions) => res.render('list', {questions}))
 })
 
+router.get('/new', (req, res) => {
+  res.render('new')
+})
+
+router.post('/', (req, res) => {
+  Question.create(req.body)
+    .then(() => res.redirect('/'))
+})
 module.exports = router
