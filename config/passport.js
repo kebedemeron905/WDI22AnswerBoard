29331aaef51cfab83677fb595b6cabc8020f37b4
Meron Kebede <kebedemeron905@gmail.com>
2018-04-10 14:01:10 -0400

    add signup

--- config/passport.js
+++ config/passport.js
@@ -1,1 +1,25 @@
-//first line
+const LocalStrategy = require('passport-local')
+const User = require('../models/user')
+
+module.exports = function (passport) {
+  passport.use('local-signup', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+  }, function (req, email, password, done) {
+    User.findOne({'local.email': email}, function (err, user) {
+      if (err) return done(err)
+      if (user) {
+        return done(null, false, req.flash('signupMessage', 'That email is already in use!'))
+      } else {
+        const newUser = new User()
+        newUser.local.email = email
+        newUser.local.password = newUser.encrypt(password)
+        newUser.save(err => {
+          if (err) throw err
+          return done(null, newUser)
+        })
+      }
+    })
+  }))
+}
