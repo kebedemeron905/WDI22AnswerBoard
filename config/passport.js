044dd7e9ad7538f11394f7d3587cf374ace6b916
Jason Toups <jason.p.toups@gmail.com>
2018-04-10 15:19:11 -0400

    set up config for log in and nav bar in layout

--- config/passport.js
+++ config/passport.js
@@ -31,4 +31,21 @@ module.exports = function (passport) {
       }
     })
   }))
+
+  passport.use('local-login', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+  }, function (req, email, password, done) {
+    User.findOne({'local.email': email}, function (err, user) {
+      if (err) return done(err)
+      if (!user) {
+        return done(null, false, req.flash('loginMessage', 'No user exists for that. Please create an account.'))
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, req.flash('loginMessage', 'Incorrect password. Please try again'))
+      }
+      return done(null, user)
+    })
+  }))
 }
