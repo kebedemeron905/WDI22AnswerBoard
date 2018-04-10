936269e720ddf9d5d26181134da645c6004b9a95
Jason Toups <jason.p.toups@gmail.com>
2018-04-10 13:42:17 -0400

    install packages, update index.js boilerplate

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 const express = require('express')
 const parser = require('body-parser')
 const hbs = require('hbs')
-const questionsController = require('./controllers/questions')
 const methodOverride = require('method-override')
 const app = express()
+const flash = require('connect-flash')
+const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const questionsController = require('./controllers/questions')
+
+app.use(morgan('dev'))
+app.use(cookieParser)
 
 app.set('view engine', 'hbs')
 app.use(parser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-
 app.use('/assets', express.static('public'))
+
+app.use(flash())
+
+const passport = require('passport')
+const passportConfig = require('./config/passport')
+passportConfig(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user
+  next()
+})
 
 app.use('/', questionsController)
 
