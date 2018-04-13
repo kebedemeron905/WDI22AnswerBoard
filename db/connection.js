bbfe5caf42f4a02765dc8f3752b65eeb6e0c881e
Jason Toups <jason.p.toups@gmail.com>
2018-04-13 11:14:14 -0400

    add mlab connection

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose')
-
 const connectionURI = `mongodb://localhost/questions_db`
 
-mongoose.connect(connectionURI)
+if (process.env.NODE_ENV == "production") {
+  mongoose.connect(process.env.MLAB_URL)
+} else {
+  mongoose.connect(connectionURI)
+}
+
 mongoose.Promise = Promise
 
 module.exports = mongoose
